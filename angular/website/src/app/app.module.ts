60920701fc8e3a4982c5baf88faa70f9187ca449
Rahul Vaidyagit <rahulnkvaidya@yahoo.co.in>
2019-02-10 22:53:34 +0530

    angular add

--- angular/website/src/app/app.module.ts
+++ angular/website/src/app/app.module.ts
@@ -1,34 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { SnotifyModule, SnotifyService, ToastDefaults } from "ng-snotify";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
-import { CKEditorModule } from "ng2-ckeditor";
-import { AdminModule } from "./admin/admin.module";
-import { NotifyService } from "./services/notify.service";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    SnotifyModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    CKEditorModule,
-    AdminModule
+    AppRoutingModule
   ],
-  providers: [
-    NotifyService,
-    { provide: "SnotifyToastConfig", useValue: ToastDefaults },
-    SnotifyService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
